fc826536c663c4697e688f0d448aa9c66189306a
kashishdosar2804-art <kashishdosar2804@gmail.com>
2025-10-01 19:00:12 +0530

    Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Experience />
+          <Projects />
+          <Contact />
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
